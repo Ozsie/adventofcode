8c311d98d6779ae10682d760c602085e84897660
Oscar Djupfeldt <oscar@djupfeldt.se>
2020-12-11 07:39:03 +0100

    Added unfinished Day 11, task 1

--- src/2020/Day11_1.kts
+++ src/2020/Day11_1.kts
@@ -6,27 +6,39 @@ File("input/2020/day11").forEachLine {
     input.add(it.toCharArray())
 }
 
-for (y in 0..input.size) {
+val yMax = input.size-1
+for (y in 0..yMax) {
     val row = input[y]
-    println(row)
-    for (x in 0..row.size) {
+    val xMax = row.size-1
+    for (x in 0..xMax) {
         val seat = row[x]
+        print(seat)
         if (seat == '.') continue
         val newState = newState(x,y,seat,input)
         row.set(x,newState)
     }
+    print(" / ")
+    print(row)
+    println("")
+}
+println("-----------------")
+for (y in 0..yMax) {
+    val row = input[y]
+    println(row)
 }
 
 fun newState(x: Int, y: Int, seat: Char, input: ArrayList<CharArray>): Char {
     var occupied = 0
-    println("Checking $x $y")
     for (aY in IntRange(y-1,y+1)) {
         for (aX in IntRange(x-1,x+1)) {
             if (aY == y && aX == x) continue
-            if (aY < 0 || aY >= input.size - 1) continue
-            if (aX < 0 || aX >= input[aY].size - 1) continue
-            println("$aX $aY")
-            if (input[aY][aX] == '#') occupied++
+            if (aY < 0) continue
+            if (aY >= input.size) continue
+            if (aX < 0) continue
+            if (aX >= input[aY].size) continue
+            if (input[aY][aX] == '#') {
+                occupied++
+            }
         }
     }
     return if (occupied == 0 && seat == 'L') '#' else if (occupied >= 4 && seat == '#') 'L' else seat

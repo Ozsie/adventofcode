02df71668ad3bd3ccf38c766bbe635e700926e94
Oscar Djupfeldt <oscar@djupfeldt.se>
2020-12-13 15:42:42 +0100

    Added Day 13 (Fast part 2)

--- src/2020/Day13_2.kts
+++ src/2020/Day13_2.kts
@@ -2,42 +2,16 @@ import java.io.File
 
 var parts = ArrayList<String>()
 File("input/2020/day13").forEachLine { parts.add(it) }
-val departureTime = parts[0].toLong()
-var next = Long.MAX_VALUE
-var id = 0L
-val buses = parts[1].split(",")
-val max = buses.filter { it != "x" }.map { it.toLong() }.max() ?: 0L
-val indexOfMax = buses.indexOf(max.toString())
-println("$max at $indexOfMax")
 
-var noMatch = true
-var t = 0L-max
-while (noMatch) {
-    t += max
-    if (t % 100000000000000L == 0L) println("At $t")
-    var sequence = MutableList<Long>(buses.size, { i -> 1L })
-    sequence.set(indexOfMax, 0L)
-    for (x in (indexOfMax+1)..buses.lastIndex) {
-        when(buses[x]) {
-            "x" -> sequence.set(x, 0L)
-            else -> {
-                val mod = (t + (x - indexOfMax)) % buses[x].toLong()
-                sequence.set(x, (t + (x - indexOfMax)) % buses[x].toLong())
-                if (mod != 0L) break
-            }
-        }
-    }
-    if (sequence.sum() > 0L) continue;
-    for (x in indexOfMax - 1 downTo 0) {
-        when(buses[x]) {
-            "x" -> sequence.set(x, 0L)
-            else -> {
-                val mod = (t - (indexOfMax - x)) % buses[x].toLong()
-                sequence.set(x, mod)
-                if (mod != 0L) break
-            }
-        }
-    }
-    noMatch = sequence.sum() != 0L
+var busTimeOffsets = ArrayList<Pair<Long,Int>>()
+parts[1].split(",").forEachIndexed { index, s -> if (s != "x") busTimeOffsets.add(Pair(s.toLong(),index)) }
+
+var (increment,_) = busTimeOffsets[0]
+
+var position = 0L
+for (i in 1..busTimeOffsets.lastIndex) {
+    val (busTime, busOffset) = busTimeOffsets[i]
+    do { position += increment } while ((position + busOffset) % busTime != 0L)
+    increment = increment * busTime
 }
-println(t)
+println(position)

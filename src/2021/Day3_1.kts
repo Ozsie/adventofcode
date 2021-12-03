b36377cdfc690b29554442d16e30cd899d93fbf7
Ozsie <oscar@djupfeldt.se>
2021-12-03 10:14:11 +0100

    Cleaning

--- src/2021/Day3_1.kts
+++ src/2021/Day3_1.kts
@@ -2,21 +2,20 @@ import java.io.File
 import kotlin.collections.HashMap
 import kotlin.collections.ArrayList
 
-var columns = HashMap<Int, ArrayList<Int>>()
+fun String.lengthOfFiltered() = Pair(filter { it == '0' }.length, filter { it == '1' }.length)
+
+var columns = ArrayList<String>()
 File("input/2021/day3").forEachLine { line ->
     line.forEachIndexed { i, c ->
-        val list = columns.getOrDefault(i, ArrayList<Int>())
-        list.add(c.digitToInt())
-        columns.put(i, list)
+        columns.getOrElse(i) { columns.add(""); "" }
+            .also { columns.set(i, it + c) }
     }
 }
 var gamma = ""
 var epsilon = ""
-columns.map {
-    val zeroes = it.value.filter { c -> c == 0 }.size
-    val ones = it.value.filter { c -> c == 1 }.size
-    gamma += if (zeroes > ones) "0" else if (ones > zeroes) "1" else "X"
-    epsilon += if (zeroes > ones) "1" else if (ones > zeroes) "0" else "X"
+columns.forEach {
+    val (zeroes, ones) = it.lengthOfFiltered()
+    gamma += if (ones > zeroes) 1 else 0
+    epsilon += if (zeroes > ones) 1 else 0
 }
-
-println("${gamma.toInt(2)*epsilon.toInt(2)}")
+println(gamma.toInt(2) * epsilon.toInt(2))
--- src/2021/Day3_2.kts
+++ src/2021/Day3_2.kts
@@ -9,27 +9,21 @@ fun ArrayList<String>.reduce(column: Int, type: Type): ArrayList<String> {
     val zeroes = filter { it[column] == '0' }.size
     val ones = size - zeroes
     return when(type) {
-        Type.O -> {
-            if (zeroes > ones) filter { it[column] == '0' }
-            else filter { it[column] == '1' }
-        }
-        Type.CO -> {
-            if (ones < zeroes) filter { it[column] == '1' }
-            else filter { it[column] == '0' }
-        }
+        Type.O -> if (zeroes > ones) filter { it[column] == '0' } else filter { it[column] == '1' }
+        Type.CO -> if (ones < zeroes) filter { it[column] == '1' } else filter { it[column] == '0' }
     } as ArrayList<String>
 }
 
-File("input/2021/day3").forEachLine { rows.add(it) }
+var oxygen = ArrayList<String>()
+var co2 = ArrayList<String>()
+File("input/2021/day3").forEachLine {
+    oxygen.add(it)
+    co2.add(it)
+}
+val cols = oxygen[0].length
 
-var oxygen = ArrayList<String>().apply { addAll(rows) }
-var co2 = ArrayList<String>().apply { addAll(rows) }
-for (col in 0..rows.size) {
-    if (oxygen.size > 1) {
-        oxygen = oxygen.reduce(col, Type.O)
-    }
-    if (co2.size > 1) {
-        co2 = co2.reduce(col, Type.CO)
-    }
+for (col in 0..cols) {
+    if (oxygen.size > 1) oxygen = oxygen.reduce(col, Type.O)
+    if (co2.size > 1) co2 = co2.reduce(col, Type.CO)
 }
 println(oxygen[0].toInt(2) * co2[0].toInt(2))

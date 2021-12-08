4e9f06e721ec7d3bdf3865bf1d8119e83170499a
Ozsie <oscar@djupfeldt.se>
2021-12-08 10:22:58 +0100

    Cleanup

--- src/2021/Day8_1.kts
+++ src/2021/Day8_1.kts
@@ -1,14 +1,11 @@
 import java.io.File
-import kotlin.collections.HashMap
-import kotlin.collections.ArrayList
 
 val signals = ArrayList<Pair<String, String>>()
-
 File("input/2021/day8").forEachLine { line ->
     val (pattern, output) = line.split("|").map { it.trim() }
     signals.add(Pair(pattern, output))
 }
-val easySignals = signals.map { it.second }.flatMap { it.split(" ") }.filter {
+println(signals.map { it.second }.flatMap { it.split(" ") }.filter {
     when(it.length) {
         2 -> true
         3 -> true
@@ -16,6 +13,4 @@ val easySignals = signals.map { it.second }.flatMap { it.split(" ") }.filter {
         7 -> true
         else -> false
     }
-}.size
-
-println(easySignals)
+}.size)

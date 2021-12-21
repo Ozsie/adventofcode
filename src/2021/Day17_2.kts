8d684d332cddaf214bc00076da9a8b83673f65a6
Ozsie <oscar@djupfeldt.se>
2021-12-21 09:22:27 +0100

    Day 17 clean up

--- src/2021/Day17_2.kts
+++ src/2021/Day17_2.kts
@@ -11,30 +11,20 @@ fun shoot(x: Int, y: Int, xRange: IntRange, yRange: IntRange): Result {
     var velocity = Pair(x,y)
     var steps = 0
     var over = false
-    while (!(position.first in xRange && position.second in yRange)) {
+    while (true) {
         steps++
-        //print("$steps: Vel: $velocity -> ")
         val nX = position.first + velocity.first
         val nY = position.second + velocity.second
         position = Pair(nX, nY)
-        //print(" $position")
         if (nY > localYMax) localYMax = nY
 
         val inX = position.first >= xRange.first && position.first <= xRange.last
         val inY = position.second >= yRange.first && position.second <= yRange.last
-        //print(" ($inX,$inY)")
-        if (inX && inY) {
-            //if (hitCount % 10 == 0)
-            //println(" ($x,$y)")
-            //else print(" ($x,$y)")
-            break
-        }
+        if (inX && inY) break
 
         val long = nX > Math.max(xRange.last, xRange.first)
         val toMuchY = nY < Math.min(yRange.last, yRange.first)
-        //print(" ($long,$toMuchY)")
         if (long || toMuchY) {
-            //println(" over at ($nX,$nY)")
             over = true
             break
         }
@@ -42,12 +32,8 @@ fun shoot(x: Int, y: Int, xRange: IntRange, yRange: IntRange): Result {
         val nVX = if (velocity.first > 0) velocity.first - 1 else if (velocity.first < 0) velocity.first + 1 else velocity.first
         val nVY = velocity.second - 1
         velocity = Pair(nVX, nVY)
-        //println()
     }
-    if (!over){
-        //println(localYMax)
-        maxYPos = max(localYMax, maxYPos)
-    }
+    if (!over) maxYPos = max(localYMax, maxYPos)
     return Result(Pair(x,y), maxYPos, !over)
 }
 
@@ -57,8 +43,6 @@ File("input/2021/day17").forEachLine { line ->
     val (yMin, yMax) = yTarget.substring(2).split("..").map { it.toInt() }.sortedBy { it }
     val xRange = xMin.toInt()..xMax.toInt()
     val yRange = yMin.toInt()..yMax.toInt()
-    //println(xRange)
-    //println(yRange)
 
     var initVel = Pair(0,0)
     var maxYPos = Int.MIN_VALUE
@@ -75,5 +59,5 @@ File("input/2021/day17").forEachLine { line ->
         }
     }
 
-    println("$maxYPos, $initVel, $hitCount")
+    println(hitCount)
 }
